481e4ca0ccd1c469ad7af227d477f175187d5767
Taing Bunsou <taing.bunsou24@kit.edu.kh>
2025-09-07 07:45:41 +0700

    Add Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './exception-filters/http.exception-filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,16 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  //? Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Simple Quote Generator API')
+    .setDescription('The Simple Quote Generator API description')
+    .setVersion('1.0')
+    .addTag('quotes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
